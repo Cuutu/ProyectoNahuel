63f4b99ea3f0173d7d1980a8147201ca01dba0b6
Cuutu <franco.l.varela99@gmail.com>
2024-11-28 03:27:43 -0300

    Hola

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js,ejs}"],
   theme: {
     extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#1a1a1a',
+        accent: '#ffd700',
+      },
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
       },
