b9f55ccd57b978079c52f4b59f85ea7914ea4c61
Cuutu <franco.l.varela99@gmail.com>
2024-11-28 03:11:10 -0300

    Arreglao

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Configuración básica
-app.use(express.static('src/public'));
+// Configuración de archivos estáticos - usando la ruta correcta
+app.use(express.static(path.join(__dirname, 'src/public')));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
-// Importar rutas
+// Rutas
 const landingRoutes = require('./src/routes/landingRoutes');
-
-// Usar rutas
 app.use('/', landingRoutes);
 
 const PORT = process.env.PORT || 3000;
