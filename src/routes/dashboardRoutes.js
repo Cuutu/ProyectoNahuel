a5ae45e295c1ba9fa2f1e521fcd2f0ae8ce85c05
Cuutu <franco.l.varela99@gmail.com>
2024-12-06 03:20:04 -0300

    Intentando arreglar la sesión

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { isAuthenticated } = require('../middleware/auth');
 
-router.get('/dashboard', isAuthenticated, (req, res) => {
+router.get('/', isAuthenticated, (req, res) => {
     res.render('dashboard/index', { 
         user: req.user,
         isAuthenticated: true
