2fe7379acd8e324f95c2fe7a5a9128d814ad9331
joaquinperez028 <tortuwu028@gmail.com>
2024-12-06 01:46:51 -0300

    ingreso con cuenta google

--- src/config/passport.js
+++ src/config/passport.js
@@ -18,7 +18,9 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL: process.env.NODE_ENV === 'production' 
+        ? 'https://tu-app-vercel.vercel.app/auth/google/callback'  // URL de producción
+        : 'http://localhost:3000/auth/google/callback',            // URL de desarrollo
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 async (accessToken, refreshToken, profile, done) => {
